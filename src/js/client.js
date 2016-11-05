e9ee89f39238fc3b35c2ba0618b69b8ed866996b
Rupesh Kumar Singh <RupeshKumar.Singh@xchanging.my.local>
2016-11-05 15:32:53 +0800

    react js webpack config

---
+++ src/js/client.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { render } from 'react-dom';
+class Layout extends React.Component{
+    render(){
+        return(
+            <div className="container">
+                <div className="row">
+                    <div className="col-xs-10 col-xs-offset-1">
+                        <h1>Hello12345!</h1>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+};
+render(<Layout/>, window.document.getElementById('app'));
