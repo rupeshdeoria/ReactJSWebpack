76e01b4e28f37a517f4d892ad9ce0f04ad3be543
Rupesh Kumar Singh <RupeshKumar.Singh@xchanging.my.local>
2016-11-12 10:07:05 +0800

    constructor in react class

--- src/js/client.js
+++ src/js/client.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { render } from 'react-dom';
 class Layout extends React.Component{
+    constructor(){
+        super();
+        this.name = "rupesh";
+    }
     render(){
         return(
             <div className="container">
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
-                        <h1>Hello12345678!</h1>
+                        <h1>Hello {this.name}</h1>
                     </div>
                 </div>
             </div>
